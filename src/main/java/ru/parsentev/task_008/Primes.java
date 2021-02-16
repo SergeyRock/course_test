3d96a6dfb5f701e9a58b8d498f0c5b99a811be0b
Sergey Oleynikov <oleynikov.sergey@uchitel-izd.ru>
2021-02-16 21:26:17 +0300

    task_008 Primes

--- src/main/java/ru/parsentev/task_008/Primes.java
+++ src/main/java/ru/parsentev/task_008/Primes.java
@@ -3,6 +3,7 @@ package ru.parsentev.task_008;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -23,6 +24,27 @@ public class Primes {
     }
 
     public List<Integer> calc() {
-        throw new UnsupportedOperationException();
+        List<Integer> primes = new ArrayList<>();
+
+        for (int i = 1; i <= limit; i++) {
+            int divCount = 0;
+            boolean isPrime = true;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divCount++;
+                    if (divCount > 2) {
+                        break;
+                    }
+                }
+            }
+
+            isPrime = divCount == 2;
+
+            if (isPrime) {
+                primes.add(i);
+            }
+        }
+        return primes;
     }
 }
--- src/test/java/ru/parsentev/task_008/PrimesTest.java
+++ src/test/java/ru/parsentev/task_008/PrimesTest.java
@@ -24,4 +24,10 @@ public class PrimesTest {
         List<Integer> result = primes.calc();
         assertThat(result, is(asList(2, 3)));
     }
+    @Test
+    public void calcTo200() {
+        Primes primes = new Primes(200);
+        List<Integer> result = primes.calc();
+        assertThat(result, is(asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199)));
+    }
 }
