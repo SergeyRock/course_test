bd80ceb6a8fc447b4148af9be2d5e1bb2471e2a8
Sergey Oleynikov <oleynikov.sergey@uchitel-izd.ru>
2021-02-17 21:41:56 +0300

    task_015 CycleShift

--- src/main/java/ru/parsentev/task_015/CycleShift.java
+++ src/main/java/ru/parsentev/task_015/CycleShift.java
@@ -1,7 +1,6 @@
 package ru.parsentev.task_015;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -14,6 +13,10 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class CycleShift {
     private static final Logger log = getLogger(CycleShift.class);
 
+    public int[] getValues() {
+        return values;
+    }
+
     private final int[] values;
 
     public CycleShift(final int[] values) {
@@ -21,6 +24,50 @@ public class CycleShift {
     }
 
     public int[] shift(int position) {
-        throw new UnsupportedOperationException();
+        if (position == 0) {
+            return values;
+        }
+
+        if (values.length == 0) {
+            return values;
+        }
+
+        if (Math.abs(position) >= values.length) {
+            position %= values.length;
+        }
+
+        for (int i = 1; i <= Math.abs(position); i++) {
+            if (position > 0) {
+                manualMoveToOnePositivePosition();
+            } else {
+                manualMoveToOneNegativePosition();
+            }
+        }
+
+        return values;
+    }
+
+    public void manualMoveToOnePositivePosition() {
+        if (values.length <= 1) {
+            return;
+        }
+
+        int lastElement = values[values.length - 1];
+        for (int i = values.length - 1; i > 0; i--) {
+            values[i] = values[i - 1];
+        }
+        values[0] = lastElement;
+    }
+
+    public void manualMoveToOneNegativePosition() {
+        if (values.length <= 1) {
+            return;
+        }
+
+        int firstElement = values[0];
+        for (int i = 0; i < values.length - 1; i++) {
+            values[i] = values[i + 1];
+        }
+        values[values.length - 1] = firstElement;
     }
 }
--- src/test/java/ru/parsentev/task_015/CycleShiftTest.java
+++ src/test/java/ru/parsentev/task_015/CycleShiftTest.java
@@ -2,7 +2,6 @@ package ru.parsentev.task_015;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import ru.parsentev.task_014.ReversePhrase;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -29,4 +28,40 @@ public class CycleShiftTest {
         int[] result = cycle.shift(2);
         assertThat(result, is(new int[] {1}));
     }
+
+    @Test
+    public void negativeShiftTest() {
+        CycleShift cycle = new CycleShift(new int[] {1, 2, 3, 4});
+        int[] result = cycle.shift(-2);
+        assertThat(result, is(new int[] {3, 4, 1, 2}));
+    }
+
+    @Test
+    public void emptyTest() {
+        CycleShift cycle = new CycleShift(new int[] {});
+        int[] result = cycle.shift(2);
+        assertThat(result, is(new int[] {}));
+    }
+
+    @Test
+    public void longShiftTest() {
+        CycleShift cycle = new CycleShift(new int[] {1, 2, 3});
+        int[] result = cycle.shift(5);
+        assertThat(result, is(new int[] {2, 3, 1}));
+    }
+
+    @Test
+    public void moveToOnePositivePositionTest() {
+        CycleShift cycle = new CycleShift(new int[] {1, 2, 3, 4});
+        cycle.manualMoveToOnePositivePosition();
+        assertThat(cycle.getValues(), is(new int[] {4, 1, 2, 3}));
+    }
+
+    @Test
+    public void moveToOneNegativePositionTest() {
+        CycleShift cycle = new CycleShift(new int[] {1, 2, 3, 4});
+        cycle.manualMoveToOneNegativePosition();
+        assertThat(cycle.getValues(), is(new int[] {2, 3, 4, 1}));
+    }
+
 }
